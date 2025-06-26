edbda80ae3ece24cc8de6db577cb5252fdff7192
segerrity <ujy8jg@virginia.edu>
2025-06-26 09:26:07 -0400

    upload button, pre-deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+'use client'
+import { useState } from 'react';
+import { FileButton, Button, Group, Text } from '@mantine/core';
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  const [file, setFile] = useState<File | null>(null);
+  return (
+    <>
+      <Group justify="center">
+        <FileButton onChange={setFile} accept="image/png,image/jpeg">
+          {(props) => <Button {...props}>Upload image</Button>}
+        </FileButton>
+      </Group>
+
+      {file && (
+        <Text size="sm" ta="center" mt="sm">
+          Picked file: {file.name}
+        </Text>
+      )}
+    </>
+  );
 }
